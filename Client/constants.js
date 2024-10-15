644e9678a08a5658442c4dc09363f33da6756f9a
tagobuyhelp <tagobuyhelp@gmail.com>
2024-10-15 21:10:43 +0530

    a link underline fixed in home page

--- Client/constants.js
+++ Client/constants.js
@@ -1,3 +1,3 @@
 // constants.js
-const API_URL = 'http://localhost:5000'; // Or load this from an environment variable
+const API_URL = 'https://server.velkiclubagentlist.com'; // Or load this from an environment variable
 module.exports = { API_URL }; // Use CommonJS export
